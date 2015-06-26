c376575b57887c6fc46bf301aaaa5c3c09c75d3f
Pablo Karlsson <Pablo.Karlsson@gmail.com>
2015-06-27 00:18:18 +0200

    Update configmanager.go

--- packages/configmanager/configmanager.go
+++ packages/configmanager/configmanager.go
@@ -1,4 +1,4 @@
-// Copyright 2015 Pablo Karlssnon. All rights reserved.
+// Copyright 2015 Pablo Karlsson. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file at https://github.com/PabloK/revel_react_boiler.
 
